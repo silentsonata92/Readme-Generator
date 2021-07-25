0b332e425da1bd612b4c9eda3afe6d9c74a749e3
silentsonata <joon.mink@yahoo.com>
2021-07-25 15:14:56 -0700

    fixed the markdown

--- markdown.js
+++ markdown.js
@@ -1,51 +1,50 @@
 function generateReadMe(data) {
   return `
-    # <${data.title}>
+# ${data.title}
 
-    ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
-    ---
-    ## Description
-    ---
-    ${data.description}
-    ---
-    ## Table of Contents
-    ---
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    ---
-    ## Installation
-    ---
-    These dependencies are need in order to run the app successfully:
-    ${data.installation}
-    ---
-    ## Usage
-    ---
-    To make app run: 
-    ${data.usage}
-    ---
-    ## License
-    ---
-    ${data.license}
-    ---
-    ## Contributing
-    ---
-    ${data.contributing}
-    ---
-    ## Tests
-    ---
-    ${data.tests}
-    ---
-    ## Questions
-    ---
-    How to reach me:
-    ${data.questions}
-    ---
-    
+![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+---
+## Description
+---
+${data.description}
+---
+## Table of Contents
+---
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+---
+## Installation
+---
+These dependencies are need in order to run the app successfully:
+${data.installation}
+---
+## Usage
+---
+To make app run: 
+${data.usage}
+---
+## License
+---
+${data.license}
+---
+## Contributing
+---
+${data.contributing}
+---
+## Tests
+---
+${data.tests}
+---
+## Questions
+---
+How to reach me:
+${data.questions}
+---
     `
-  }
+}
   
 module.exports = generateReadMe
